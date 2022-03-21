84b9b42ae112f67cf46802bb8b588c8e850f91ee
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-03-20 20:04:14 -0500

    tag, a few recipes

--- src/main/java/com/jetpacker06/unburned/base/item/ItemsAll.java
+++ src/main/java/com/jetpacker06/unburned/base/item/ItemsAll.java
@@ -13,6 +13,8 @@ public class ItemsAll {
             DeferredRegister.create(ForgeRegistries.ITEMS, Unburned.MOD_ID);
     //begin items
     public static final RegistryObject<Item> FIREPROOFING = ITEMS.register("fireproofing", () -> new FireproofingItem(new Item.Properties().tab(ModCreativeModeTab.UNBURNED)));
+    public static final RegistryObject<Item> NETHER_COMPOUND = ITEMS.register("nether_compound", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.UNBURNED)));
+
     //end items
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
