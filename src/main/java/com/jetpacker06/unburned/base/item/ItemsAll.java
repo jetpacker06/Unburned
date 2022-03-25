92211d15d36a6c6065bff560a6e7e1a0e1955c06
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-03-25 17:33:11 -0500

    Data overhaul II

--- src/main/java/com/jetpacker06/unburned/base/item/ItemsAll.java
+++ src/main/java/com/jetpacker06/unburned/base/item/ItemsAll.java
@@ -3,6 +3,7 @@ package com.jetpacker06.unburned.base.item;
 import com.jetpacker06.unburned.Unburned;
 import com.jetpacker06.unburned.base.item.advanced.FireproofingItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +13,7 @@ public class ItemsAll {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Unburned.MOD_ID);
     //begin items
-    public static final RegistryObject<Item> FIREPROOFING = ITEMS.register("fireproofing", () -> new FireproofingItem(new Item.Properties().tab(ModCreativeModeTab.UNBURNED)));
+    public static final RegistryObject<Item> FIREPROOFING = ITEMS.register("fireproofing", () -> new FireproofingItem(new Item.Properties().tab(ModCreativeModeTab.UNBURNED).craftRemainder(Items.GLASS_BOTTLE)));
     public static final RegistryObject<Item> NETHER_COMPOUND = ITEMS.register("nether_compound", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.UNBURNED)));
 
     //end items
