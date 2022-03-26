4f698c4a7bea5473e78a5a7661437ce29c6cc19b
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-03-26 17:47:50 -0500

    Update to 1.18.2

--- src/main/java/com/jetpacker06/unburned/base/tag/TagsAll.java
+++ src/main/java/com/jetpacker06/unburned/base/tag/TagsAll.java
@@ -4,6 +4,7 @@ import com.jetpacker06.unburned.Unburned;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
@@ -11,23 +12,23 @@ import net.minecraftforge.common.Tags;
 public class TagsAll {
     public static class Blocks {
 
-        private static Tags.IOptionalNamedTag<Block> tag(String name) {
-            return BlockTags.createOptional(new ResourceLocation(Unburned.MOD_ID, name));
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(Unburned.MOD_ID, name));
         }
-        private static Tags.IOptionalNamedTag<Block> forgeTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
     public static class Items {
-        public static final Tags.IOptionalNamedTag<Item> NETHER_COMPOUND_CRAFTING_MATERIALS = tag("nether_compound_crafting_materials");
+        public static final TagKey<Item> NETHER_COMPOUND_CRAFTING_MATERIALS = tag("nether_compound_crafting_materials");
 
 
 
-        private static Tags.IOptionalNamedTag<Item> tag(String name) {
-            return ItemTags.createOptional(new ResourceLocation(Unburned.MOD_ID, name));
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(Unburned.MOD_ID, name));
         }
-        private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
